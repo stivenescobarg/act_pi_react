de5acd6747a6d556d7c4a6d04bafd038e7747475
Darly Johana Zambrano Zambrano <zambrano1985darly@gmail.com>
2025-08-25 15:43:37 -0500

    Creé un componente HabitItem el cual, es el encargado de mostrar un hábito específico y ofrecer la interacción para que el usuario cambie su estado entre completado y pendiente por medio de un boton

---
+++ components/HabitItem.tsx
@@ -0,0 +1,20 @@
+// components/HabitItem.tsx
+"use client";
+
+import { Habit } from "../lib/types";
+
+interface HabitItemProps {
+  habit: Habit;
+  toggleHabit: (id: number) => void;
+}
+
+export default function HabitItem({ habit, toggleHabit }: HabitItemProps) {
+  return (
+    <div className={habit-item ${habit.completed ? "completed" : "pending"}}>
+      <span>{habit.name}</span>
+      <button onClick={() => toggleHabit(habit.id)}>
+        {habit.completed ? "Desmarcar" : "Completar"}
+      </button>
+    </div>
+  );
+}
